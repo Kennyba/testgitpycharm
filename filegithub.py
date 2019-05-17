a3a1f00e3d15691c50624e72ca4acd08d2beeb4f
Kennyba <k.bienaime@hotmail.com>
2019-05-17 06:55:12 -0400

    I wrote in my local with Pycharm

--- filegithub.py
+++ filegithub.py
@@ -1,3 +1,11 @@
 print("I am from Github")
 print("I Wrote this on my local")
 print("I am trying another line in the local")
+
+def test(x):
+    for i in range(x):
+        print("This is a test "+str(i))
+    return("The end")
+
+
+test(5)
